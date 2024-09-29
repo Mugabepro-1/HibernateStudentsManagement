9c82dcc68a60b0cf7b00f69d97419569d4d2caf2
promesse <mugabepromesse@gmail.com>
2024-09-29 22:28:48 +0200

    first commit

---
+++ src/app/oop/exercises/DatabaseConnect.java
@@ -0,0 +1,12 @@
+package app.oop.exercises;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnect {
+    public static void main(String[] args){
+        String url = "";
+        String user = "";
+        String password = "";
+    }
+}
