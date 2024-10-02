b6eccc65936a9c0d6b4f187d00e9b59edd373482
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-02 15:18:06 +0200

    added the pom.xml file

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,24 @@ public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first name: ");
+        System.out.print("Enter first name: ");
         String firstName = scanner.next();
 
-        System.out.println("Enter last name: ");
-        String lastName = scanner.nextLine();
+        System.out.print("Enter last name: ");
+        String lastName = scanner.next();
 
-        System.out.println("Enter date of birth in yyyy-mm-dd format: ");
-        String dob = scanner.nextLine();
+        System.out.print("Enter date of birth in yyyy-mm-dd format: ");
+        String dob = scanner.next();
         Date dateOfBirth = Date.valueOf(dob);
 
 
-        System.out.println("Enter your school name: ");
-        String school = scanner.nextLine();
+        System.out.print("Enter your school name: ");
+        String school = scanner.next();
 
-        System.out.println("Enter your combination: ");
-        String combination = scanner.nextLine();
+        System.out.print("Enter your combination: ");
+        String combination = scanner.next();
 
-        System.out.println("Enter your level: ");
+        System.out.print("Enter your level: ");
         String level = scanner.nextLine();
 
         Student student = new Student(firstName, lastName, dateOfBirth, school, combination, level);
