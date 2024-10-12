e10f055757cdf6c2e0ae514026858530805e49f2
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-12 22:30:43 +0200

    debugged some errors

--- Crud/src/Main.java
+++ Crud/src/Main.java
@@ -1,6 +1,7 @@
 import java.sql.Date;
 import java.util.Scanner;
 import people.DatabaseManager;
+import people.ReadDatabase;
 import people.Student;
 
 public class Main {
@@ -8,22 +9,23 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first name: ");
-        String firstName = scanner.next();
-        System.out.print("Enter last name: ");
-        String lastName = scanner.next();
-        System.out.print("Enter date of birth in yyyy-mm-dd format: ");
-        String dob = scanner.next();
-        Date dateOfBirth = Date.valueOf(dob);
-        System.out.print("Enter your school name: ");
-        String school = scanner.next();
-        System.out.print("Enter your combination: ");
-        String combination = scanner.next();
-        System.out.print("Enter your level: ");
-        String level = scanner.next();
-        Student student = new Student(firstName, lastName, dateOfBirth, school, combination, level);
-        DatabaseManager dbManager = new DatabaseManager();
-        dbManager.insertStudent(student);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter first name: ");
+//        String firstName = scanner.next();
+//        System.out.print("Enter last name: ");
+//        String lastName = scanner.next();
+//        System.out.print("Enter date of birth in yyyy-mm-dd format: ");
+//        String dob = scanner.next();
+//        Date dateOfBirth = Date.valueOf(dob);
+//        System.out.print("Enter your school name: ");
+//        String school = scanner.next();
+//        System.out.print("Enter your combination: ");
+//        String combination = scanner.next();
+//        System.out.print("Enter your level: ");
+//        String level = scanner.next();
+//        Student student = new Student(firstName, lastName, dateOfBirth, school, combination, level);
+//        DatabaseManager dbManager = new DatabaseManager();
+//        dbManager.insertStudent(student);
+
     }
 }
