baadce860b864e09a25e96e19880ffb15b96ae36
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-14 19:58:41 +0200

    set filepath as input

--- Crud/src/people/CsvReader.java
+++ Crud/src/people/CsvReader.java
@@ -4,5 +4,5 @@ import java.util.Scanner;
 
 public class CsvReader {
     Scanner scanner = new Scanner(System.in);
-    //Left with setting up the file path as input and execute elvin's codes to read csv file
+    String filePath = scanner.next();
 }
