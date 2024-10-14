d0b46baa88d0f8e8db66f2c380612013f6f33899
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-14 20:07:46 +0200

    added the data insertion method

--- Crud/src/people/CsvReader.java
+++ Crud/src/people/CsvReader.java
@@ -1,8 +1,23 @@
 package people;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class CsvReader {
     Scanner scanner = new Scanner(System.in);
     String filePath = scanner.next();
+    private static final String URL = "jdbc:postgresql://localhost:5432/students";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "mupro";
+
+    public Connection connect() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD );
+    }
+    public void insertDataFromCsv(String filePath){
+        String sql = "INSERT INTO student(firstName, lastName, dateOfBirth, school, combination, level) VALUES(?, ?, ?, ?, ?, ?)";
+    }
+
 }
